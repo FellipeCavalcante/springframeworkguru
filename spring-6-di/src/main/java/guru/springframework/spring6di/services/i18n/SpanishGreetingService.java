f3f95d10df6b3773fa45c6cf06e2d7cb9d920047
FellipeCavalcante <azevedoaranha1@gmail.com>
2024-09-13 15:16:35 -0300

    up projects

---
+++ spring-6-di/src/main/java/guru/springframework/spring6di/services/i18n/SpanishGreetingService.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring6di.services.i18n;
+
+import guru.springframework.spring6di.services.GreetingService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("ES")
+@Service("i18NService")
+public class SpanishGreetingService implements GreetingService {
+
+    public String sayGreeting() { return "Hola mundo -ES"; }
+}
