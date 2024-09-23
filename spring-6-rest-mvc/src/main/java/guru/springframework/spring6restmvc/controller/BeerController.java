4e51bfdf5c1fc2ac275eb3df4b17735645cf0bc7
FellipeCavalcante <azevedoaranha1@gmail.com>
2024-09-23 15:19:35 -0300

    add MockMVC test with Mockito

--- spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -3,6 +3,7 @@ package guru.springframework.spring6restmvc.controller;
 import guru.springframework.spring6restmvc.model.Beer;
 import guru.springframework.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -14,7 +15,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Slf4j
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/beer")
 public class BeerController {
@@ -64,7 +65,7 @@ public class BeerController {
     @RequestMapping(value = "{beerId}", method = RequestMethod.GET)
     public Beer getBeerById(@PathVariable("beerId") UUID beerId) {
 
-        log.debug("Get Beer by Id - in controller -1234 vasco");
+        log.debug("Get Beer by Id - in controller - 1234 vasco");
 
         return beerService.getBeerById(beerId);
     }
